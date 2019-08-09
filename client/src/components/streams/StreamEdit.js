b825c3670ef08176893ad47d0efc43e4fb86a3d4
Samuel Git Pessoal <samuel.belo@al.infnet.edu.br>
2019-08-09 11:31:07 -0300

    getting records from state

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux'
+
+
 
 const StreamEdit = (props) => {
     console.log(props)
     return <div>StreamEdit</div>
 }
 
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] }
+}
 
-export default StreamEdit
+
+export default connect(mapStateToProps)(StreamEdit)
