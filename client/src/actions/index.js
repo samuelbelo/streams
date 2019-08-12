776d73119cddc96158d55b87b539eb12045e55c5
Samuel Git Pessoal <samuel.belo@al.infnet.edu.br>
2019-08-12 15:15:07 -0300

    refact fetch api for edit w/o drop any props

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -50,7 +50,8 @@ export const deleteStream = (id) => async dispatch => {
     dispatch({ type: DELETE_STREAM, payload: id })
 }
 export const editStream = (id, formValues) => async dispatch => {
-    const response = await streams.put(`/streams/${id}`, formValues)
+    const response = await streams.patch(`/streams/${id}`, formValues)
 
     dispatch({ type: EDIT_STREAM, payload: response.data })
+    history.push('/')
 }
--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,6 +1,7 @@
+import _ from 'lodash'
 import React from 'react';
 import { connect } from 'react-redux'
-import { fetchStream } from '../../actions'
+import { fetchStream, editStream } from '../../actions'
 import StreamForm from './StreamForm'
 
 
@@ -10,8 +11,8 @@ class StreamEdit extends React.Component {
         this.props.fetchStream(this.props.match.params.id)
     }
 
-    onSubmit = (formValues) => {
-        console.log(formValues)
+    onSubmit = formValues => {
+        this.props.editStream(this.props.match.params.id, formValues)
     }
 
     render() {
@@ -31,7 +32,7 @@ class StreamEdit extends React.Component {
             <div>
                 <h3>Edit a stream</h3>
                 <StreamForm onSubmit={this.onSubmit}
-                    initialValues={this.props.stream} />
+                    initialValues={_.pick(this.props.stream, 'title', 'description')} />
             </div>
         )
     }
@@ -42,4 +43,4 @@ const mapStateToProps = (state, ownProps) => {
 }
 
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit)
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit)
