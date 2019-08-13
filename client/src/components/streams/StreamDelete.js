0ca0a141086688dca194b78cb9eb71acd61e0077
Samuel Git Pessoal <samuel.belo@al.infnet.edu.br>
2019-08-13 11:58:51 -0300

    refactoring delete function for use of portals

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import Modal from '../Modal'
 
 const StreamDelete = () => {
-    return <div>StreamDelete</div>
-}
+
+    const Actions = (
+        <React.Fragment>
+            <button className="ui button negative">Delete</button>
+            <button className="ui button">Cancel</button>
+        </React.Fragment>
+    )
+
+    return (
+        <div>
+            <Modal
+                title="Delete Stream"
+                content="Are you sure you want to delete?"
+                action={Actions}
+            />
+        </div>
+    );
+};
 
 
 export default StreamDelete
